dc39fae7c40c2650638d1a8eeb8366ae4850d32f
Anuj Shelke <anujshelke18@gmail.com>
2026-07-16 19:48:32 +0530

    program to swap Array elements at a particular index

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import javax.swing.*;
+import java.sql.SQLOutput;
+import java.util.*;
+//Swap Array elements at a particular index
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello world This is First commit for the DSA in java Project ");
+        int []arr = {1,2,3,4,5};
+        int temp;
+        System.out.println("Enter the number to swap ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Enter the index at which you want to swap with ");
+        int m  = sc.nextInt();
+
+        for(int i = 0 ; i < 5 ; i++){
+            temp = arr[n];
+            arr[n] = arr[m];
+            arr[m] = temp;
+
+        }
+        System.out.println(Arrays.toString(arr));
+
+
     }
 }
