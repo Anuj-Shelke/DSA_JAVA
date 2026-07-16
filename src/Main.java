0e8301db97f39e2e34b6756ed51e901c431ed9d8
Anuj Shelke <anujshelke18@gmail.com>
2026-07-16 21:38:01 +0530

    Program for linear search of elements

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
-//Program to find Maximum Element in an array
-import java.util.Scanner;
+//Program for linear search of elements ;
+import java.util.*;
 public class Main{
     public static void main(String[] args){
-        int [] arr = {1,2,10,7,5,6};
-        int max = 0 ;
-        for(int i = 0 ; i< 5 ; i++ ){
+        System.out.println("Enter the element to search in the array ");
+        Scanner sc = new Scanner(System.in);
+        int element = sc.nextInt();
+        int [] arr = {1,2,3,4,5,6,7,8,9};
+        int result = search(arr,element);
+        System.out.println("The array element "+element+" is found at index : "+result);
 
-            if(max< arr[i+1]){
-                max = arr[i];
+    }
+    static int search(int[] arr, int target ){
+        if(arr.length == 0 ){
+            return -1;
+        }
+        for(int i = 0 ; i< arr.length; i++ ){
+            if(arr[i] == target){
+                return i;
             }
 
         }
-        System.out.println("The Maximum element of the array is "+max);
-
+        return -1;
     }
 }
