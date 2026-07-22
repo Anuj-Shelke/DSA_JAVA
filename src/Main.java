da219aa110bbfa2929e825022b317f78cafa175c
Anuj Shelke <anujshelke18@gmail.com>
2026-07-22 23:06:32 +0530

    Cyclic sort Leet Code

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,34 @@ import java.util.Arrays;
 
 public class Main{
     public static void main(String[] args){
-        int[] arr={1,4,5,3,2,0};
-        cyclic_sort(arr);
+        int[] arr ={1,3,2,5,2};
+        int result = cyclic_sort(arr);
         System.out.println(Arrays.toString(arr));
+        System.out.println(result );
 
 
     }
-    static void cyclic_sort(int[] arr){
-       int i = 0;
+    static int cyclic_sort(int[] arr){
+        int i = 0 ;
         while(i < arr.length){
-
-            int correct = arr[i] ;
-            if(arr[i] != arr[correct]){
+            int correct = arr[i]-1;
+            if(arr[correct]!=arr[i]){
                 swap(arr,i,correct);
             }
-            else{
-                i++ ;
-
+            else {
+                if (i!=correct ) {
+                    return arr[i];
+                }
+                i++;
             }
 
+
         }
+        return -1;
     }
-    static void swap(int[] arr, int first , int second){
-        int temp = arr[first];
-        arr[first] = arr[second] ;
-        arr[second] = temp ;
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first] ;
+        arr[first ]= arr[second];
+        arr[second] = temp;
     }
 }
