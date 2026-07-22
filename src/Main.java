e8e132c9b71a770a084a374012013e51f6830501
Anuj Shelke <anujshelke18@gmail.com>
2026-07-22 22:35:00 +0530

    Cyclic Sort practice1

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,31 @@
-import java.util.*;
+import java.util.Arrays;
+
 public class Main{
     public static void main(String[] args){
-        int [] arr = {1,5,4,3,2};
+        int[] arr={1,4,5,3,2,0};
         cyclic_sort(arr);
         System.out.println(Arrays.toString(arr));
+
+
     }
     static void cyclic_sort(int[] arr){
-        int i = 0;
-        while(i < arr.length ) {
-            int correct = arr[i] - 1;
+       int i = 0;
+        while(i < arr.length){
+
+            int correct = arr[i] ;
             if(arr[i] != arr[correct]){
                 swap(arr,i,correct);
             }
             else{
-                i++;
+                i++ ;
+
             }
+
         }
     }
     static void swap(int[] arr, int first , int second){
         int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
+        arr[first] = arr[second] ;
+        arr[second] = temp ;
     }
 }
