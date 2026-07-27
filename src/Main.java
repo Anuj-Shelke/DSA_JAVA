85360d0bf20575cb95330d23f78171a94564523d
Anuj Shelke <anujshelke18@gmail.com>
2026-07-27 11:04:57 +0530

    Program for Printing Star Pattern No 3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
-//Program for Printing Star Pattern No 2 ;
+//Program for Printing Star Pattern No 3 ;
 public class Main{
     public static void main(String[] args){
-        pattern2(4);
+        pattern2(5);
     }
     public static void pattern2(int n){
-        for(int row = 0 ; row <= n ; row++){
-            for(int coln = 0 ; coln <= n-1 ; coln++){
-                System.out.print("* ");
+        for(int row = n ; row >0  ; row--){
+            for(int coln = 0 ; coln < row ; coln++){
+                System.out.print("*");
             }
             System.out.println();
         }
