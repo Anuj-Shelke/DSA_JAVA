c2552f75029f229ae8ea522573cf7bdbcba5dc41
Anuj Shelke <anujshelke18@gmail.com>
2026-07-27 14:34:16 +0530

    //Program for Printing Numbered Pattern No 4 ;

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,21 @@
 public class Main{
     public static void main(String[] args){
         pattern2(5);
+        pattern3(4);
+
     }
     public static void pattern2(int n){
         for(int row = 0 ; row <=n; row++){
             for(int coln = 1 ; coln <= row ; coln++){
-                System.out.print(coln);
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern3(int n){
+        for(int row =n ; row >0 ; row--){
+            for(int coln = 1 ; coln <= row ; coln++){
+                System.out.print("*");
             }
             System.out.println();
         }
