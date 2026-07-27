a49839accd36f2148bd7a43e7d4d55fc76fae3c8
Anuj Shelke <anujshelke18@gmail.com>
2026-07-27 11:01:26 +0530

    Code For Priting Pattern 2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
-//Program for printing Star Pattern...............
+//Program for Printing Star Pattern No 2 ;
 public class Main{
     public static void main(String[] args){
-        pattern1(4);
-
+        pattern2(4);
     }
-    public static void pattern1(int n){
-        for(int row = 0 ; row <=n ; row++){
-            for(int coln = 0 ; coln <= row ; coln++){
-                System.out.printf("* ");
+    public static void pattern2(int n){
+        for(int row = 0 ; row <= n ; row++){
+            for(int coln = 0 ; coln <= n-1 ; coln++){
+                System.out.print("* ");
             }
             System.out.println();
         }
