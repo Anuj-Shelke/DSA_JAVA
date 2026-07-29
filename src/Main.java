5b236b1434678a1c10bb5696c2675f4b40375cde
Anuj Shelke <anujshelke18@gmail.com>
2026-07-29 23:07:47 +0530

    Code to print factorial of a given number

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
-//Code to print factors of a given number ;
+//Code to print factorial  of a given number ;
 public class Main{
     public static void main(String[] args){
+        int fact = 1;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number to know the factor of ");
         int n = sc.nextInt();
         for(int i = 1 ; i< n ; i++){
-            if(n%i ==0 ){
-                System.out.printf("%d\n",i);
-            }
-
+        fact = fact * i;
         }
+        System.out.printf("The factorial of given number is %d",fact);
     }
 }
