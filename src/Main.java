4f674fba3c54569c98e967335966cd9edfb7f4a2
Anuj Shelke <anujshelke18@gmail.com>
2026-07-29 08:58:16 +0530

    Program to check if a number is palindrome  ;

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-//Program For demonstrating Recursion ;
+//Program to check if a number is palindrome  ;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
@@ -6,10 +6,18 @@ public class Main {
         int temp= 0 ;
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int save =n ;
         while(n!= 0 ){
             temp = temp*10+(n%10);
             n = n/10;
         }
         System.out.println("The reverse of the number is "+temp);
+        if(save == temp){
+            System.out.println("The number is a palindrome ");
+        }
+        else{
+            System.out.println("The number is not a palindrome ");
+        }
     }
+
 }
