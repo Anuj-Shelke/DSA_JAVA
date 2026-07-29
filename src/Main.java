e2085f6449ea6a78022f2c952bfb3cd01bdeb206
Anuj Shelke <anujshelke18@gmail.com>
2026-07-29 23:23:58 +0530

    //Code to print the Result of power and base   ;

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,17 @@
 import java.util.Scanner;
 
-//Code to check if given number is a perfect Number  ;
+//Code to print the Result of power and base   ;
 public class Main{
     public static void main(String[] args){
-        int factor_sum = 0 ;
+        System.out.println("Enter the value of base ");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number to check if it is a perfect Number  ");
-        int n = sc.nextInt();
-        for(int i = 1 ; i< n ; i++){
-        if(n%i ==0){
-            factor_sum = factor_sum + i ;
+        int base = sc.nextInt() ;
+        System.out.println("Enter the value of power ");
+        int power = sc.nextInt();
+        int result = 1;
+        for(int i = 0 ; i < power ; i++){
+            result  = result * base ;
         }
-        }
-        System.out.println("The sum of the factors is "+factor_sum);
-        if(factor_sum == n ){
-            System.out.println("The given number is a perfect number ");
-        }
-        else{
-            System.out.println("the number is not a perfect number ");
-        }
+        System.out.println("The result is "+result);
     }
 }
