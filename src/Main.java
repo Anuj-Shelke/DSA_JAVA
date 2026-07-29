53b6d154d5063e67db25330b0474ba3e53326620
Anuj Shelke <anujshelke18@gmail.com>
2026-07-29 09:35:21 +0530

    Program to Enter Base and power and enter their result  ;

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,20 @@
-//Program to check if a number is palindrome  ;
+//Program to Enter Base and power and enter their result  ;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter number to reverse ");
-        int temp= 0 ;
+        int count = 0;
+        int temp = 1;
+        System.out.println("Enter base ");
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int save =n ;
-        while(n!= 0 ){
-            temp = temp*10+(n%10);
-            n = n/10;
+        int base = sc.nextInt();
+        System.out.println("Enter power ");
+        int power = sc.nextInt();
+        while(count != power){
+            temp = temp *base ;
+            count++;
+
         }
-        System.out.println("The reverse of the number is "+temp);
-        if(save == temp){
-            System.out.println("The number is a palindrome ");
-        }
-        else{
-            System.out.println("The number is not a palindrome ");
-        }
+        System.out.printf("The result is %d",temp);
     }
 
 }
