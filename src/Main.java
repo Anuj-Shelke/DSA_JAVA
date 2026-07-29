8a4b72fdb46b24431f344637aca99747c449e392
Anuj Shelke <anujshelke18@gmail.com>
2026-07-29 23:18:17 +0530

    Code to check if given number is a perfect number

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
 
-//Code to print factorial  of a given number ;
+//Code to check if given number is a perfect Number  ;
 public class Main{
     public static void main(String[] args){
-        int fact = 1;
+        int factor_sum = 0 ;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number to know the factor of ");
+        System.out.println("Enter number to check if it is a perfect Number  ");
         int n = sc.nextInt();
         for(int i = 1 ; i< n ; i++){
-        fact = fact * i;
+        if(n%i ==0){
+            factor_sum = factor_sum + i ;
         }
-        System.out.printf("The factorial of given number is %d",fact);
+        }
+        System.out.println("The sum of the factors is "+factor_sum);
+        if(factor_sum == n ){
+            System.out.println("The given number is a perfect number ");
+        }
+        else{
+            System.out.println("the number is not a perfect number ");
+        }
     }
 }
