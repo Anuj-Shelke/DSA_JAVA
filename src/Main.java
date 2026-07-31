0fbdbfe48cbe48cba42ab0184075567f5016fe64
Anuj Shelke <anujshelke18@gmail.com>
2026-07-31 14:49:57 +0530

    Code to print Addtion of digits of a number and reverse the digit using Loops

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,28 @@
 import java.util.*;
-//Code to print Factorial of a number using recursion
+//Code to Print sum of digits of a number No recursion approach first  ;
 public class Main{
 
     public static void main(String[] args ) {
-        int result = sum(2);
-        System.out.println("The Sum of given natural number is :  "+result);
-    }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number to add digits of ");
+        int num = sc.nextInt();
+        int num2 = num ;
+        int r = 0;
 
-    static int  sum(int n ){
-        if(n==1){
-            return 1 ;
+        while(num >  0) {
+            r =r + (num%10);
+            System.out.println("The digits are "+r);
+            num  = num/10;
+
         }
-         return n + sum(n-1);
+        int r1 = 0 ;
+        System.out.println("The addition of the digits is "+r);
+        while(num2 > 0) {
+            r1 = r1 * 10 + (num2 % 10);
+            num2 = num2/10;
 
+        }
+        System.out.println("The reverse of the digit is "+r1);
 
 
     }
