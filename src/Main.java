1b26cda9a3e1b8b60e72604d0dc5e485925d0151
Anuj Shelke <anujshelke18@gmail.com>
2026-07-31 10:23:17 +0530

    Code to print 1 to 5 and 5 to 1 using recursion

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.util.*;
+//Code to print 1 to 5 and 5 to 1 using recursion
 public class Main{
-    public static void main(String[] args ){
-        System.out.println("Enter a number to reverse : \n");
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int digit = 0;
-        while(num > 0){
-            digit = (digit*10)+(num%10);
-            num = (num/10);
+    static void func(int n ){
+        if(n==0){
+            return ;
         }
-        System.out.println("The reverse of the number "+digit);
+        System.out.println(n);
+        func(n-1);
+        System.out.println(n);
+    }
+    public static void main(String[] args ) {
+        func(5);
     }
 }
