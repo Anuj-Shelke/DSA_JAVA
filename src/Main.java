8c0b5fe2a35708f99254142ba51a5c955d045f5d
Anuj Shelke <anujshelke18@gmail.com>
2026-07-31 14:02:36 +0530

    //Code to print Factorial of a number using recursion

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 import java.util.*;
-//Code to print 1 to 5 and 5 to 1 using recursion
+//Code to print Factorial of a number using recursion
 public class Main{
-    static void func(int n ){
-        if(n==0){
-            return ;
-        }
-        System.out.println(n);
-        func(n-1);
-        System.out.println(n);
-    }
+
     public static void main(String[] args ) {
-        func(5);
+        int result = fact(5);
+        System.out.println("The factorial of number is "+result);
+    }
+
+    static int  fact(int n ){
+        if(n==1){
+            return 1 ;
+        }
+         return n * fact(n-1);
+
+
+
     }
 }
