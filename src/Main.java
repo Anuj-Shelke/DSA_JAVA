6ff4e34ae5f608b4496407bf785270a1fdfc9e39
Anuj Shelke <anujshelke18@gmail.com>
2026-09-06 08:47:52 +0530

    Visiting DSA Again Starting With HashMap

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,11 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Main{
-    public static boolean ispali(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]","");
-        s = s.toLowerCase();
-        String rev = new StringBuilder(s).reverse().toString();
-        return s.equals(rev);
-    }
     public static void main(String[] args){
-        System.out.println("Enter String to check if it is a Palindrome");
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        boolean result = ispali(name);
-        System.out.println(result);
-
+        HashMap<Integer,String> map = new HashMap<>();
+        map.put(1,"Anuj");
+        map.put(2,"Kavya");
+        map.put(3,"Divya");
+        System.out.println(map);
 
     }
 }
