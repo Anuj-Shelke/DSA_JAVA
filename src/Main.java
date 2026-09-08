73f234ff8908d1c32349c6d6572f004b366d02d4
Anuj Shelke <anujshelke18@gmail.com>
2026-09-08 22:24:08 +0530

    REVISION CODE FOR LINEAR SEARCH

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.*;
 public class Main{
     public static void main(String[] args){
-        HashMap<Integer,String> map = new HashMap<>();
-        map.put(1,"Anuj");
-        map.put(2,"Kavya");
-        map.put(3,"Divya");
-        System.out.println(map);
+       int []arr = {1,2,3,4,5,6};
+       System.out.println("Enter the Element You Want to Search For ");
+       Scanner sc = new Scanner(System.in);
+       int found = 0 ;
+       int element = sc.nextInt();
+       for(int i = 0 ; i < arr.length; i++ ){
+           if(arr[i] == element){
+               System.out.println("Element is found at index "+i);
+               found = 1;
+           }
+
+           }
+
+        if(found != 1  ) {
+            System.out.println("The element is not found ");
+        }
+       }
 
     }
-}
