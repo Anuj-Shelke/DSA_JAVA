f0144a1f4761b6ed0cd3575a3f4537d3513c5f9d
Anuj Shelke <anujshelke18@gmail.com>
2026-09-09 10:20:46 +0530

    Binary Search Code Revision JAVA

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,31 @@
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.*;
 public class Main{
     public static void main(String[] args){
-       int []arr = {1,2,3,4,5,6};
-       System.out.println("Enter the Element You Want to Search For ");
-       Scanner sc = new Scanner(System.in);
-       int found = 0 ;
-       int element = sc.nextInt();
-       for(int i = 0 ; i < arr.length; i++ ){
-           if(arr[i] == element){
-               System.out.println("Element is found at index "+i);
-               found = 1;
-           }
+        int found = 0;
+        int [] arr = {1,2,3,4,5,6};
+        System.out.println("Enter the element to search for ");
+        Scanner sc = new Scanner(System.in);
+        int element = sc.nextInt();
+        int end = arr.length-1;
+        int start = 0 ;
+        while(end>= start){
+            int mid = (end+start)/2;
+            if(arr[mid] == element ){
+                System.out.printf("The element is found at index %d",mid);
+                found = 1;
+                break;
 
-           }
-
-        if(found != 1  ) {
-            System.out.println("The element is not found ");
+            }
+            else if(arr[mid] < element){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
         }
-       }
-
+        if(found == 0){
+            System.out.println("The element is not their  ");
+        }
     }
+
+}
